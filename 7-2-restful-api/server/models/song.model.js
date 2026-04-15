3eb369cddf8f6c7b00b8687d3fa3e5bca76dafd8
Mohammed Alorf <mohammed1alorf@gmail.com>
2026-04-15 08:26:33 +0300

    Add song schema and model

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
 // db schema
+const songSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    artist: { type: String, required: true, trim: true },
+    year: { type: Number, min: 0, max: 9999 },
+  },
+  { timestamps: true }
+);
 
 export const Song = mongoose.model("Song", songSchema);
